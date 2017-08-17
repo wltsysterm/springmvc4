ddf0b0ae30615961ffaf616e3835ad40945a3cd4
魏霖涛 <3263138624@qq.com>
2017-08-17 16:53:40 +0800

    Initial commit

---
+++ src/main/java/com/springmvc/controller/TestController.java
@@ -0,0 +1,29 @@
+package com.springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("test")
+public class TestController {
+    @RequestMapping("do")
+    @ResponseBody
+    public void doSomething(){
+        System.out.println("11111");
+    }
+
+    @RequestMapping("do1")
+    public ModelAndView doSomething1(){
+        System.out.println("22222");
+        return new ModelAndView("");
+    }
+
+    @RequestMapping("do2")
+    @ResponseBody
+    public String doSomething2(){
+        System.out.println("11111");
+        return "测试不返回页面";
+    }
+}
